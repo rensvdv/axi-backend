2a8fc00334728f9d09872c414444b6a9003c9084
Maissan,Casper C.A <C.Maissan@student.fontys.nl>
2024-03-14 14:43:12 +0100

    Feedback en vragen klasses gemaakt

---
+++ AxiBusinessLogicLayer/Vragen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AxiBusinessLogicLayer
+{
+    public class Vragen
+    {
+        public string Kwestie {  get; private set; }
+        public string Antwoord { get; private set; }
+
+        public Vragen(string kwestie, string antwoord)
+        {
+            Kwestie = kwestie;
+            Antwoord = antwoord;
+        }
+    }
+}
