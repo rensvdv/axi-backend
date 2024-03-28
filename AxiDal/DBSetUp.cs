3470edf50ebdcbf69be47c647b046b19f287bb80
Maissan,Casper C.A <C.Maissan@student.fontys.nl>
2024-03-28 13:27:42 +0100

    Start ORM implementation

---
+++ AxiDal/DBSetUp.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using AxiInterfaces.DTO;
+
+public class SetUp : DbContext
+{
+    public string DbPath { get; }
+
+    public SetUp()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "blogging.db"); // "blogging.db" moet nog aangepast worden + Uitzoeken wat de path hoort te zijn.
+    }
+
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
+
+public class FeedbackDTO
+{
+
+
+}
