8297684b992bf39207a14292ab0a0a842e824cf9
Amin El Abdellaoui <i467072@fhict.nl>
2024-04-11 09:45:45 +0200

    Minor pull fix

--- axi-backend/Controllers/FeedbackController.cs
+++ axi-backend/Controllers/FeedbackController.cs
@@ -19,16 +19,11 @@ namespace axi_backend.Controllers
         }
 
         [HttpGet("getmijnfeedback/{feedbackid}")]
-        public List<Feedback> GetMijnFeedback(int id)
+        public List<Feedback> GetMijnFeedback(int feedbackid)
         {
-            //return _container.GetMijnFeedback(id);
+            return _container.GetMijnFeedback(feedbackid);
             //Mock return
-            return new List<Feedback> {
-                new Feedback(1, "feedback van gebruiker 4 over gebruiker 1", null, true, new Gebruiker(new Profiel()) {Id = 4, Naam = "Gebruiker 4"}, new Gebruiker(new Profiel()) {Id = 1, Naam = "Gebruiker 1" }),
-                new Feedback(2, "feedback van gebruiker 3 over gebruiker 1", null, true, new Gebruiker(new Profiel()) {Id = 3, Naam = "Gebruiker 3"}, new Gebruiker(new Profiel()) {Id = 1, Naam = "Gebruiker 1" }),
-                new Feedback(3, "feedback van gebruiker 2 over gebruiker 1", null, true, new Gebruiker(new Profiel()) {Id = 2, Naam = "Gebruiker 2"}, new Gebruiker(new Profiel()) {Id = 1, Naam = "Gebruiker 1" }),
-
-                };
+            /* return new List<Feedback> { new Feedback(1, "hi", null, true, null, null) }; */
         }
 
         [HttpPost("maakfeedback")]
